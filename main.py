91ddd4ddbded855fd0db67932d7fbea2d7ece4ba
sudoRicheek <richeekdas2001@gmail.com>
2020-03-19 01:44:16 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 18 16:01:32 2020
+
+@author: Richeek Das
+"""
+
+
+from mememaker_gui import *
+
+object_for_frontend = mememaker_gui()
+object_for_frontend.open_image("")
